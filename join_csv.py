e118d49729db194a2a918425ab018e6d13fe58e0
Adiwijaya Satria Nusantara <adihanyu@gmail.com>
2024-04-21 12:33:01 +0700

    update combined data

--- join_csv.py
+++ join_csv.py
@@ -2,19 +2,20 @@ import pandas as pd
 import os
 from tqdm import tqdm
 
-# cats = os.listdir('output_csv/')
-# df = pd.DataFrame()
+cats = os.listdir('output_csv/')
+df = pd.DataFrame()
 
-# for i in tqdm(range(len(cats))):
-#     years = os.listdir(f'output_csv/{cats[i]}/')
+for i in tqdm(range(len(cats))):
+    years = os.listdir(f'output_csv/{cats[i]}/')
 
-#     for year in years:
-#         # print(year)
-#         temp = pd.read_csv(f'output_csv\\{cats[i]}\\{year}')
-#         # print(df.head(5))
-#         df = df._append(temp, ignore_index=True)
+    for year in years:
+        # print(year)
+        temp = pd.read_csv(f'output_csv\\{cats[i]}\\{year}')
+        # print(df.head(5))
+        df = df._append(temp, ignore_index=True)
 
 # print(cat)
-df = pd.read_csv('combined_data.csv')
-print(df.head(5).T)
-# df.to_csv('combined_data.csv', index=False)
+# df = pd.read_csv('combined_data.csv')
+# print(df.head(5).T)
+df.drop(['Unnamed: 0'], axis=1, inplace=True)
+df.to_csv('combined_data.csv', index=False)
